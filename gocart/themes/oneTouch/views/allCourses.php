1274acf79092bee74d4a9fcb7158e5f5bc9bdaf8
junaidkhalil <j.khalil@weprosolutions.co.uk>
2013-09-03 15:45:59 +0500

    All Deleted

--- gocart/themes/oneTouch/views/allCourses.php
+++
@@ -1,8 +0,0 @@
- <?php include('mainHeader.php'); ?>  
-
-     <?php include('header.php'); ?> 
-        <?php //include_once('coursesContent.php'); ?> 
-        <?php include_once('course_catogery.php'); ?> 
-                </div>
-            </div>
-     <?php include_once('footer.php'); ?> 
--- gocart/themes/oneTouch/views/portfilo.php
+++
@@ -1,7 +0,0 @@
- <?php include('mainHeader.php'); ?>  
-
-     <?php include('header.php'); ?> 
-        <?php include_once('coursesContent.php'); ?> 
-                </div>
-            </div>
-     <?php include_once('footer.php'); ?> 
